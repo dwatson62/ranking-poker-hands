cba03cc9ef454f24458fdc5544a159867709f814
Daryl Watson <darylwatson@Daryls-MacBook-Air.local>
2015-10-20 13:52:20 +0100

    Can check for a single pair

--- lib/hand_evaluator.rb
+++ lib/hand_evaluator.rb
@@ -1,4 +1,12 @@
 class HandEvaluator
   def return_stronger_hand(left, right)
+    return left if check_for_pair(left) > check_for_pair(right)
+    right
+  end
+
+  def check_for_pair(hand)
+    hand = hand.join.chars
+    answer = hand.select { |x| x =~ /[0-9]/ && hand.count(x) > 1 }
+    answer[0] || '0'
   end
 end
