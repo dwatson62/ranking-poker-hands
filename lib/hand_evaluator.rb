1a20b60aef495d770cfb1cb311dcda08b0dc9462
Daryl Watson <darylwatson@Daryls-MacBook-Air.local>
2015-10-21 01:48:57 +0100

    Refactored flush with duck typing

--- lib/hand_evaluator.rb
+++ lib/hand_evaluator.rb
@@ -7,13 +7,13 @@ class HandEvaluator
 
   def return_stronger_hand(left, right)
     set_hands(left, right)
-    return hand_with_straight_flush if hand_with_straight_flush != nil
-    return hand_with_x_of_a_kind(4) if hand_with_x_of_a_kind(4) != nil
-    return hand_with_full_house if hand_with_full_house != nil
-    return hand_with_flush if hand_with_flush != nil
-    return hand_with_straight if hand_with_straight != nil
-    return hand_with_x_of_a_kind(3) if hand_with_x_of_a_kind(3) != nil
-    return hand_with_x_of_a_kind(2) if hand_with_x_of_a_kind(2) != nil
+    return hand_with_straight_flush if hand_with_straight_flush
+    return hand_with_x_of_a_kind(4) if hand_with_x_of_a_kind(4)
+    return hand_with_full_house if hand_with_full_house
+    return hand_with_flush if hand_with_flush
+    return hand_with_straight if hand_with_straight
+    return hand_with_x_of_a_kind(3) if hand_with_x_of_a_kind(3)
+    return hand_with_x_of_a_kind(2) if hand_with_x_of_a_kind(2)
     compare_kickers
   end
 
@@ -22,9 +22,16 @@ class HandEvaluator
     @right = right
   end
 
+  def check_ranking(rank)
+    return left if rank.check(left) && !rank.check(right)
+    return right if !rank.check(left) && rank.check(right)
+    return compare_kickers if rank.check(left) && rank.check(right)
+    nil
+  end
+
   def hand_with_full_house
-    return left if check_full_house(left) == true && check_full_house(right) == false
-    return right if check_full_house(left) == false && check_full_house(right) == true
+    return left if check_full_house(left) && !check_full_house(right)
+    return right if !check_full_house(left) && check_full_house(right)
     return compare_kickers if check_full_house(left) && check_full_house(right)
     nil
   end
@@ -34,33 +41,25 @@ class HandEvaluator
   end
 
   def hand_with_straight_flush
-    return left if check_straight_flush(left) == true && check_straight_flush(right) == false
-    return right if check_straight_flush(left) == false && check_straight_flush(right) == true
+    return left if check_straight_flush(left) && !check_straight_flush(right)
+    return right if !check_straight_flush(left) && check_straight_flush(right)
     return compare_kickers if check_straight_flush(left) && check_straight_flush(right)
     nil
   end
 
   def hand_with_flush
-    return left if check_flush(left) == true && check_flush(right) == false
-    return right if check_flush(left) == false && check_flush(right) == true
-    return compare_kickers if check_flush(left) && check_flush(right)
-    nil
+    check_ranking(Flush)
   end
 
   def hand_with_straight
-    return left if check_straight(left) == true && check_straight(right) == false
-    return right if check_straight(left) == false && check_straight(right) == true
+    return left if check_straight(left) && !check_straight(right)
+    return right if !check_straight(left) && check_straight(right)
     return compare_kickers if check_straight(left) && check_straight(right)
     nil
   end
 
   def check_straight_flush(hand)
-    check_flush(hand) && check_straight(hand)
-  end
-
-  def check_flush(hand)
-    suit = hand[0][1]
-    hand.select { |x| x[1] == suit }.length == CARDS
+    Flush.check(hand) && check_straight(hand)
   end
 
   def check_straight(hand)
@@ -76,10 +75,10 @@ class HandEvaluator
   def hand_with_x_of_a_kind(x)
     left_x_of_kind = get_x_of_a_kind(left, x)
     right_x_of_kind = get_x_of_a_kind(right, x)
-    return if left_x_of_kind.length == 0 && right_x_of_kind.length == 0
+    return if left_x_of_kind.none? && right_x_of_kind.none?
     return left if left_x_of_kind.length > right_x_of_kind.length
     return right if left_x_of_kind.length < right_x_of_kind.length
-    return hand_with_stronger_x(left_x_of_kind, right_x_of_kind)
+    hand_with_stronger_x(left_x_of_kind, right_x_of_kind)
   end
 
   def get_x_of_a_kind(hand, x)
@@ -102,6 +101,40 @@ class HandEvaluator
       return left if sorted_left[x] > sorted_right[x]
       return right if sorted_left[x] < sorted_right[x]
     end
+  end
+
+end
+
+class Flush
+
+  def self.check(hand)
+    suit = hand[0][1]
+    hand.select { |x| x[1] == suit }.length == 5
+  end
+
+end
+
+class Straight
+
+  def self.check(hand)
+    check_hand = adjust_values(hand)
+    check_hand.each_cons(2).all? { |x, y| y == x.next }
+  end
+
+end
+
+# class FullHouse
+
+#   def self.check(hand)
+#     get_x_of_a_kind(hand, 3).any? && get_x_of_a_kind(hand, 2).any?
+#   end
+
+# end
+
+class StraightFlush
+
+  def self.check(hand)
+    Flush.check(hand) && Straight.check(hand)
   end
 
 end
